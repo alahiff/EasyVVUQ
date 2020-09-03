bb0a31ff75d1b4928607f18b6af463a284a4200f
Vytautas Jancauskas <unaudio@gmail.com>
2020-09-03 10:08:28 +0200

    bug in execute_slurm

--- easyvvuq/actions/execute_slurm.py
+++ easyvvuq/actions/execute_slurm.py
@@ -49,7 +49,9 @@ class ActionStatusSLURM():
 
     def __init__(self, script, script_name, target_dir):
         self.script = script
-        self.script_name = script_name
+        self.script_name = os.path.join(target_dir, script_name)
+        with open(self.script_name, 'w') as fd:
+            fd.write(self.script)
         self.target_dir = target_dir
         self._started = False
 
